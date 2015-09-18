129d441aafe6794fd62a244182c9ded2d91a0b08
David Moss <drkjam@gmail.com>
2015-09-18 21:46:10 +0100

    - added example API usage to PKG-INFO

--- setup.py
+++ setup.py
@@ -34,7 +34,47 @@ setup(
     url='https://github.com/drkjam/pydba/',
     download_url='https://pypi.python.org/pypi/pydba/',
     description='Tools for DBAs (with deadlines)!',
-    long_description='',
+    long_description="""`pydba` is provides a handy API to common database admin operations from Python.
+
+API Usage
+---------
+
+Basic imports and class constructor usage.
+
+    >>> from pydba import PostgresDB
+    >>> db = PostgresDB()
+    >>> db.available()
+    True
+    >>> db.names()
+    ['postgres']
+
+Database creation and deletion.
+
+    >>> db.create('foo')
+    >>> db.names()
+    ['postgres', 'foo']
+    >>> db.rename('foo', 'bar')
+    >>> db.names()
+    ['postgres', 'bar']
+
+Database backup and restore.
+
+    >>> db.dump('bar', 'bar.backup')
+    >>> db.drop('bar')
+    >>> db.names()
+    ['postgres']
+    >>> db.restore('bar', 'bar.backup')
+    >>> db.names()
+    ['postgres', 'bar']
+
+Querying and removing shutting down database connections.
+
+    >>> db.connections('postgres')
+    [{'datname': 'postgres', 'state': 'idle', 'pid': 8937, 'psql', 'query': '', 'usename': 'drkjam', ...}]
+    >>> db.kill_connections('postgres')
+    >>> db.connections('postgres')
+    []
+""",
     license='MIT License',
     platforms='POSIX',
     cmdclass={'test': PyTest},
